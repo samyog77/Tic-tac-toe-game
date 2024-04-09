8f5aa774ad45ea263dc98e78f4199a6f2694d1b3
samyog <samyog.dc@gmail.com>
2024-04-09 18:59:49 +1000

    My first commit

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import {useState} from 'react';
+
+import Player from './components/Player.jsx';
+import GameBoard from './components/GameBoard.jsx';
+import Log from './components/Log.jsx';
+
+
+function App() {
+  const [gameTurns, setGameTurns]=useState([]);
+  const [activePlayer, setActivePlayer]=useState('X');
+
+  function handleSelectSquare(rowIndex, colIndex){
+    setActivePlayer((curActivePlayer)=> (curActivePlayer==='X'?'O':'X'));
+    setGameTurns((prevTurns) =>{
+      let currentPlayer = 'X';
+      
+      if (prevTurns.Length > 0 && prevTurns[0].player ==='X'){
+        currentPlayer ='O';
+      }
+      const updatedTurns = [
+        {square: {row:rowIndex,col: colIndex}, player: currentPlayer}, ...prevTurns,
+      ];
+
+      return updatedTurns;
+
+    });
+  }
+
+  return (
+   <main>
+    <div id="game-container">
+      <ol id="players" className ="highlight-player">
+        <Player initialName ="player 1"
+        symbol ="X" 
+        isActive ={activePlayer ==='X'}/>
+        <Player initialName ="player 2" 
+        symbol ="0" 
+        isActive={activePlayer==='O'}/>
+      </ol>
+      <GameBoard onSelectSquare = {handleSelectSquare} turns={gameTurns}/>
+    </div>
+
+      <Log/>
+   </main>
+  )
+}
+
+export default App
